4ad064f604f2124f87944615b5219c7c5dfe5963
ruchirrmittal <ruchirmittal24@gmail.com>
2023-08-26 23:56:34 +0530

    asda

--- jukebox.py
+++ jukebox.py
@@ -1,36 +1,36 @@
-# albums = [
-#     ("Welcome to my Nightmare", "Alice Cooper", 1975,
-#      [
-#          (1, "Welcome to my Nightmare"),
-#          (2, "Devil's Food"),
-#          (3, "The Black Widow"),
-#          (4, "Some Folks"),
-#          (5, "Only Women Bleed"),
-#      ]
-#      ),
-#     ("Bad Company", "Bad Company", 1974,
-#      [
-#          (1, "Can't Get Enough"),
-#          (2, "Rock Steady"),
-#          (3, "Ready for Love"),
-#          (4, "Don't Let Me Down"),
-#          (5, "Bad Company"),
-#          (6, "The Way I Choose"),
-#          (7, "Movin' On"),
-#          (8, "Seagull"),
-#      ]
-#      ),
-#     ("Nightflight", "Budgie", 1981,
-#      [
-#          (1, "I Turned to Stone"),
-#          (2, "Keeping a Rendezvous"),
-#          (3, "Reaper of the Glory"),
-#          (4, "She Used Me Up"),
-#      ]
-#      ),
-# ]
+albums = [
+    ("Welcome to my Nightmare", "Alice Cooper", 1975,
+     [
+         (1, "Welcome to my Nightmare"),
+         (2, "Devil's Food"),
+         (3, "The Black Widow"),
+         (4, "Some Folks"),
+         (5, "Only Women Bleed"),
+     ]
+     ),
+    ("Bad Company", "Bad Company", 1974,
+     [
+         (1, "Can't Get Enough"),
+         (2, "Rock Steady"),
+         (3, "Ready for Love"),
+         (4, "Don't Let Me Down"),
+         (5, "Bad Company"),
+         (6, "The Way I Choose"),
+         (7, "Movin' On"),
+         (8, "Seagull"),
+     ]
+     ),
+    ("Nightflight", "Budgie", 1981,
+     [
+         (1, "I Turned to Stone"),
+         (2, "Keeping a Rendezvous"),
+         (3, "Reaper of the Glory"),
+         (4, "She Used Me Up"),
+     ]
+     ),
+]
 
-from prac import albums
+
 
 SONGS_LIST_INDEX=3
 SONG_TITLE=1
--- practice.py
+++ practice.py
@@ -79,56 +79,107 @@
 # del lis[:stop]
 # print(lis)
 
-data=[1,3,6,22,55,3,10,6,6,33,6,10,444,24,5,3,22,555,33,22,66,77,88,99,100,111,122,434,22,22]
+# data=[1,3,6,22,55,3,10,6,6,33,6,10,444,24,5,3,22,555,33,22,66,77,88,99,100,111,122,434,22,22]
 
-# for index in range(len(data)-1,-1,-1):
-#     if data[index]<10 or data[index]>100:
-#         del data[index]
+# # for index in range(len(data)-1,-1,-1):
+# #     if data[index]<10 or data[index]>100:
+# #         del data[index]
 
-# print(data)        
+# # print(data)        
 
-# top_index=len(data)-1
+# # top_index=len(data)-1
 
-# for index,value in enumerate(reversed(data)):
-#     if value<10 or value>100:
-#         print(top_index-index,value)
-#         del data[top_index-index]
+# # for index,value in enumerate(reversed(data)):
+# #     if value<10 or value>100:
+# #         print(top_index-index,value)
+# #         del data[top_index-index]
 
-# print(data)        
+# # print(data)        
 
-# lists=[[1,2,3,44,6,3],[3,2,4,2,44,3]]
+# # lists=[[1,2,3,44,6,3],[3,2,4,2,44,3]]
 
-# for numbers_l in lists:
-#     print(numbers_l,end="-------")
-#     for numbers in numbers_l:
-#         print(numbers,end=" ")
-menu = [
-    ["egg", "bacon"],
-    ["egg", "sausage", "bacon"],
-    ["egg", "spam"],
-    ["egg", "bacon", "spam"],
-    ["egg", "bacon", "sausage", "spam"],
-    ["spam", "bacon", "sausage", "spam"],
-    ["spam", "sausage", "spam", "bacon", "spam", "tomato", "spam"],
-    ["spam", "egg", "spam", "spam", "bacon", "spam"],
+# # for numbers_l in lists:
+# #     print(numbers_l,end="-------")
+# #     for numbers in numbers_l:
+# #         print(numbers,end=" ")
+# menu = [
+#     ["egg", "bacon"],
+#     ["egg", "sausage", "bacon"],
+#     ["egg", "spam"],
+#     ["egg", "bacon", "spam"],
+#     ["egg", "bacon", "sausage", "spam"],
+#     ["spam", "bacon", "sausage", "spam"],
+#     ["spam", "sausage", "spam", "bacon", "spam", "tomato", "spam"],
+#     ["spam", "egg", "spam", "spam", "bacon", "spam"],
+# ]
+
+# for meals in menu:
+#     if "spam" in meals:
+#         co=meals.count("spam")
+#         if co>1:
+#             for i in range(co):
+#                 meals.remove("spam")
+#             print("{} \n".format(meals))   
+#             for items in meals:
+#                 print(items,sep=", \n") 
+
+#         else:
+#             meals.remove("spam")
+#             print("{} \n".format(meals)) 
+#             for items in meals:
+#                 print(items,end=",") 
+#     else:
+#         print("{} \n".format(meals))   
+#         for items in meals:
+#                 print(items,end=",")               
+
+# list=['1','2','3','3','4']
+
+# list1=",".join(list)
+# print(list1)
+
+# list2=list1.split(",")
+# print(list2)
+
+# for index in range(len(list2)):
+#     list2[index]=
+
+albums = [
+    ("Welcome to my Nightmare", "Alice Cooper", 1975,
+     [
+         (1, "Welcome to my Nightmare"),
+         (2, "Devil's Food"),
+         (3, "The Black Widow"),
+         (4, "Some Folks"),
+         (5, "Only Women Bleed"),
+     ]
+     ),
+
+    ("Bad Company", "Bad Company", 1974,
+     [
+         (1, "Can't Get Enough"),
+         (2, "Rock Steady"),
+         (3, "Ready for Love"),
+         (4, "Don't Let Me Down"),
+         (5, "Bad Company"),
+         (6, "The Way I Choose"),
+         (7, "Movin' On"),
+         (8, "Seagull"),
+     ]
+     ),
+
+    ("Nightflight", "Budgie", 1981,
+     [
+         (1, "I Turned to Stone"),
+         (2, "Keeping a Rendezvous"),
+         (3, "Reaper of the Glory"),
+         (4, "She Used Me Up"),
+     ]
+     ),
 ]
 
-for meals in menu:
-    if "spam" in meals:
-        co=meals.count("spam")
-        if co>1:
-            for i in range(co):
-                meals.remove("spam")
-            print("{} \n".format(meals))   
-            for items in meals:
-                print(items,end=",") 
 
-        else:
-            meals.remove("spam")
-            print("{} \n".format(meals)) 
-            for items in meals:
-                print(items,end=",") 
-    else:
-        print("{} \n".format(meals))   
-        for items in meals:
-                print(items,end=",")               
+for name,artist,year,songs in albums:
+    print(" Album : {} , Artist : {} , year {}".format(name,artist,year))
+    for index,song in enumerate(songs):
+        print("{}: {} ".format(index+1,song))

3326c603edd28218a9611873541f820582f99bc2
ruchirrmittal <ruchirmittal24@gmail.com>
2023-09-01 21:58:22 +0530

    01/09/2023

--- pra.py
+++ pra.py
@@ -1,7 +1,15 @@
-for num in range(1,30):
-    if num>1:
-        for i in range(2,num):
-            if (num%i)==0:
-                break
-        else:
-         print(num) 
+# for num in range(1,30):
+#     if num>1:
+#         for i in range(2,num):
+#             if (num%i)==0:
+#                 break
+#         else:
+#          print(num) 
+
+
+sum=0
+for i in range(0,7,1):
+    if not i%2==0:
+        sum+=i
+
+print(sum)

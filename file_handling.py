8cdc7f7b46515cee450778ac253c2e3b1310c7fc
ruchirrmittal <ruchirmittal24@gmail.com>
2023-10-03 17:30:02 +0530

    file_io_03_10_2023

--- file_handling.py
+++ file_handling.py
@@ -254,12 +254,194 @@ data = [
 #     for row in reader:
 #         print(row)
 
+# Quote_nonnumeric is used to not let the numeric data to be returned as string
 
-import csv
+# import csv
 
-cereals='cereal_grains.csv'
+# cereals='cereal_grains.csv'
 
-with open(cereals,encoding='utf-8') as cereals_file:
-    data=csv.reader(cereals_file,quoting=csv.QUOTE_NONNUMERIC)
-    for row in data:
-        print(row)
+# with open(cereals,encoding='utf-8') as cereals_file:
+#     data=csv.reader(cereals_file,quoting=csv.QUOTE_NONNUMERIC)
+#     for row in data:
+#         print(row)
+
+
+# delimiter
+
+# import csv
+
+# with open('country_info.txt',encoding='utf-8',newline='') as fi:
+#     country_data=csv.reader(fi)
+#     for row in country_data:
+#         print(row)
+
+# program above returns each row of the data as a single string and to differentiate it we use delimiter
+# import csv
+
+# with open('country_info.txt',encoding='utf-8',newline='') as fi:
+#     country_data=csv.reader(fi,delimiter='|')
+#     for row in country_data:
+#         print(row)
+
+
+# sniffer class
+
+# import csv
+
+# with open('country_info.txt',encoding='utf-8',newline='') as country_file:
+#     sample=country_file.read()
+#     country_dialect=csv.Sniffer().sniff(sample)
+#     country_file.seek(0)  #this is used because in line 292 the file is completely read until the end and it sends the cursor back to the start
+    
+#     country_data=csv.reader(country_file,dialect=country_dialect)
+#     for row in country_data:
+#         print(row)
+
+# print("*" * 80)
+# #watch lecture 269 to understand this part given below.
+
+# attributes = [
+#     'delimiter',
+#     'doublequote',
+#     'escapechar',
+#     'lineterminator',
+#     'quotechar',
+#     'quoting',
+#     'skipinitialspace',
+# ]
+ 
+# # for attribute in attributes:
+# #     print(f'{attribute}: {getattr(country_dialect, attribute)}')
+
+
+# for attribute in attributes:
+#     print(f'{attribute}: {repr(getattr(country_dialect, attribute))}')
+
+
+# writing a csv file
+
+# import csv
+
+# cereals = [
+#     ["Barley", 556, 1.7, 32.9, 10.1, 13.8],
+#     ["Durum", 339, 5, 27.4, 4.09, 9.7],
+#     ["Fonio", 240, 1, 4, 1.7, 0.05],
+#     ["Maize", 442, 7.4, 37.45, 6.15, 11.03],
+#     ["Millet", 484, 2, 37.9, 13.4, 9.15],
+#     ["Oats", 231, 9.2, 35.1, 10.3, 3.73],
+#     ["Rice (Brown)", 346, 2.8, 38.1, 9.9, 0.8],
+#     ["Rice, (White)", 345, 3.6, 37.6, 5.4, 0.1],
+#     ["Rye", 422, 2, 31.4, 18.2, 21.2],
+#     ["Sorghum", 316, 3, 37.8, 9.92, 9.15],
+#     ["Triticale", 338, 1.81, 36.6, 19, 0.9],
+#     ["Wheat", 407, 1.2, 27.8, 12.9, 13.8],
+# ]
+
+# column_headings = ["Cereal", "Calories", "Fat", "Protein", "Fibre", "Vitamin E"]
+
+# output_filename = 'my_cereals.csv'
+
+
+# with open(output_filename,'w',encoding='utf-8',newline='') as cer_file:
+#     writer=csv.writer(cer_file,quoting=csv.QUOTE_NONNUMERIC)
+#     writer.writerow(column_headings)   #this prints just one line whereas the rows print multiples
+#     writer.writerows(cereals)
+
+
+# dictreader class in csv
+
+# import csv
+
+# cer_file='cereal_grains.csv'
+
+# with open(cer_file,encoding='utf-8',newline='') as data:
+#     reader=csv.DictReader(data)
+#     for row in reader:
+#         print(row)
+
+# country challenge using dict reader
+
+# import csv
+# country_file='country_info.txt'
+
+# countries={}
+
+# with open(country_file,encoding='utf-8',newline='') as data:
+#     sample=data.read()
+#     co_dialect=csv.Sniffer().sniff(sample)
+#     data.seek(0)
+#     reader=csv.DictReader(data,dialect=co_dialect)
+#     for row in reader:
+#         countries[row['Country'].casefold()]=row
+        
+# while True:    
+#     Name=input("Enter the name of the country \n").casefold()
+
+#     if Name in countries:
+#         A1=countries[Name]['Capital']
+#         print(f"The Capital of {Name} is {A1}")
+#     else:
+#         print("Invalid")
+#         break
+
+# import csv
+# country_file='country_info.txt'
+
+# dialect=csv.excel
+# dialect.delimiter='|'
+
+# countries={}
+
+# with open(country_file,encoding='utf-8',newline='') as data:
+#     heading=data.readline().strip('\n').split(dialect.delimiter)
+#     for index,value in enumerate(heading):
+#         heading[index]=value.casefold()
+# #the purpose of this program is to have your own custom headers rather than creating a completely new dict
+#     reader=csv.DictReader(data,dialect=dialect,fieldnames=heading)
+#     for row in reader:
+#         countries[row['country'].casefold()]=row
+
+
+# reading and writing        IMP
+
+# import csv
+
+# ordering = ['Country', 'Gold', 'Silver', 'Bronze', 'Rank']
+# # Note the lack of 'Total' in `ordering`
+
+# with open('OlympicMedals_2020.csv', encoding='utf-8', newline='') as data, open('medals_dict.py', 'w', encoding='utf-8') as output_file:
+#     # Write the first part of the code (excluding the actual data)
+#     print('import csv', file=output_file)
+#     print(file=output_file)
+#     print('medals_table = [', file=output_file)
+
+#     reader = csv.DictReader(data)
+#     # Read each row from the CSV file, as a dictionary,
+#     # and produce a new dictionary containing the key/value
+#     # pairs we want, in the order we want.
+#     for row_dict in reader:
+#         new_dict = {}
+#         # Only print the values for the keys we want
+#         # (in the order we want them).
+#         for key in ordering:
+#             value = row_dict[key]
+#             if value.isnumeric():
+#                 value = int(value)
+#             new_dict[key.casefold()] = value
+
+#         # print the dictionary to the output file
+#         # (indented by 4 spaces, with a trailing comma).
+#         print(f'    {new_dict},', file=output_file)
+
+#     # All the data rows have been written, print the terminating ]
+#     print(']', file=output_file)
+#     print(file=output_file)  # and finish with a blank line.
+
+
+# dictwriter
+
+# open medals_dict.py
+
+# zip function
+
+
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,6 +1,1 @@
-table='tablee.txt'
-
-with open(table,mode='w') as ta:
-    for i in range(3):
-        for j in range(1,10):
-            print(f"{i}x{j}={i*j}",file=ta)
+csv.excel()
